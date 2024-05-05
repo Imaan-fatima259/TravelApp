5db4e0588390ecec3f396ca8bdc7c11f33e0f10f
Imaan-fatima259 <160249870+Imaan-fatima259@users.noreply.github.com>
2024-05-05 23:40:21 +0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -11,20 +11,13 @@ const getReactNativePersistence = (storage) => {
 };
 
 const firebaseConfig = {
-  //   apiKey: "AIzaSyDQa43QukLrFMqbX0iiLWZbnozchjBz8Oo",
-  //   authDomain: "firstpro-c93e8.firebaseapp.com",
-  //   projectId: "firstpro-c93e8",
-  //   storageBucket: "firstpro-c93e8.appspot.com",
-  //   messagingSenderId: "984624672033",
-  //   appId: "1:984624672033:web:d66b70d67a04cb4370a260",
-  //   measurementId: "G-VHB9Z36FSW",
-
-  apiKey: "AIzaSyBiwFMH90jrn5e9mLUnnWu-BR2wPXRP95s",
-  authDomain: "project-d8063.firebaseapp.com",
-  projectId: "project-d8063",
-  storageBucket: "project-d8063.appspot.com",
-  messagingSenderId: "621886595684",
-  appId: "1:621886595684:web:cc831d9ba67bb0e2a8f571",
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
 };
 
 // Initialize Firebase
